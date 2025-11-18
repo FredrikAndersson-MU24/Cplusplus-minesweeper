2337ba64f0843c7449169c4005d17e05a9d06f55
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-11-18 09:22:19 +0100

    refactor code to function

--- main.cpp
+++ main.cpp
@@ -1,59 +1,67 @@
 #include <iostream>
 #include <cmath>
+#include <memory>
 
-
-
-
-int main ()
+void initializeGameboard(const std::shared_ptr<int>& num_cells)
 {
-    int num_cells = 100;
-    int numbers[num_cells + 1];
-    char row = 'A';
-    int col = 1;
-    for (int i = 1; i <= num_cells; i++)
+    int numbers[*num_cells + 1];
+    char rowLabel = 'A';
+    int colLabel = 1;
+    for (int i = 1; i <= *num_cells; i++)
     {
         numbers[i] = i;
     }
-    // Print column numbers
+
+    // Print column labels
     std::cout << " ";
-    for ( int i = 1; i <= static_cast<int>(sqrt(num_cells)); i++)
+    for ( int i = 1; i <= static_cast<int>(sqrt(*num_cells)); i++)
     {
-        std::cout << "   " << col;
-        if (i == static_cast<int>(sqrt(num_cells))) {
-        std::cout << std::endl;
+        std::cout << "   " << colLabel;
+        if (i == static_cast<int>(sqrt(*num_cells))) {
+            std::cout << std::endl;
         }
-        col++;
+        colLabel++;
     }
+
     // Print top outline
     std::cout << "  |--";
-    for ( int j = 1; j <= (static_cast<int>(sqrt(num_cells)) - 1); j++)
+    for ( int j = 1; j <= (static_cast<int>(sqrt(*num_cells)) - 1); j++)
     {
         std::cout << "-|--";
     }
     std::cout << "-|" << std::endl;
 
     // Print cell grid
-    for (int i = 1; i <= num_cells; i++)
+    for (int i = 1; i <= *num_cells; i++)
     {
-        if (i % static_cast<int>(sqrt(num_cells)) == 0)
+        if ((i - 1) % static_cast<int>(sqrt(*num_cells)) == 0) // Print row label and first col cell
+        {
+            std::cout << rowLabel << " | X | ";
+        } else if (i % static_cast<int>(sqrt(*num_cells)) != 0) // Print i:th col cell
+        {
+            std::cout << "X" << " | ";
+        } else // Print last col cell of row
         {
             std::cout << "X" << " |";
             std::cout << std::endl;
             std::cout << "  |--";
-            for ( int j = 1; j <= (static_cast<int>(sqrt(num_cells)) - 1); j++)
+            for ( int j = 1; j <= (static_cast<int>(sqrt(*num_cells)) - 1); j++)
             {
-            std::cout << "-|--";
+                std::cout << "-|--";
             }
             std::cout << "-|" << std::endl;
-            row++;
-        } else if ((i - 1) % static_cast<int>(sqrt(num_cells)) == 0)
-        {
-            std::cout << row <<" | X | ";
-        } else
-        {
-            std::cout << "X" << " | ";
+            rowLabel++;
         }
     }
+}
+
+
+int main ()
+{
+    std::shared_ptr<int> num_cells = std::make_shared<int>();
+    std::cout << "Select grid size" << std::endl;
+    std::cin >> *num_cells; // TODO Implement input validation. Except only integers that are perfect squares
+    initializeGameboard(num_cells);
 
     return 0;
 }
