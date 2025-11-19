56d1752f2fb938f154ac8ac3ef2901c9ac30255c
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2025-11-19 15:52:21 +0100

    Add while loop for testin

--- main.cpp
+++ main.cpp
@@ -8,14 +8,26 @@
 int main ()
 {
     int num_cells;
+    std::shared_ptr<bool> running = std::make_shared<bool>(true);
+    std::cout << "RUNNING : " << *running << std::endl;
 
     std::cout << "Select grid size" << std::endl;
     std::cin >> num_cells; // TODO Implement input validation. Except only integers that are perfect squares
     std::shared_ptr<GameBoard> game_board = std::make_shared<GameBoard>(num_cells);
     game_board->initGameBoard();
     game_board->printGameBoard();
-    char chars[] = {'B', '2'};
-    game_board->findCell(chars);
+
+    char chars[] = "C3";
+    while (running)
+    {
+        std::cout << "ENTER : " << std::endl;
+
+        std::cin >> chars;
+     // game_board->findCell(chars);
+    game_board->userChoice(chars, 1, running);
+    // std::cout << "RUNNING : " << *running << std::endl;
+    }
+
 
     return 0;
 }
